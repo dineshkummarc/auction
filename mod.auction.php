528551d5248589f13237a788dae4fa0d5563a346
Andrew Weaver <aweaver@brandnewbox.co.uk>
2011-10-28 21:36:49 +0100

    Added first pass at summary tag

--- mod.auction.php
+++ mod.auction.php
@@ -10,6 +10,23 @@ class Auction {
 		$this->EE =& get_instance();
 	}
 
+	public function summary() {
+		
+		$tagdata = $this->EE->TMPL->tagdata;
+		
+		// Build array of our variables
+		$data = array(
+			"current_bid" => "0.00",
+			"total_bids" => 0
+		);
+		
+		// Construct $variables array for use in parse_variables method
+		$variables = array();
+		$variables[] = $data;
+	
+		return $this->EE->TMPL->parse_variables( $tagdata, $variables );
+	}
+
 }
 /* End of file mod.auction.php */
 /* Location: /system/expressionengine/third_party/auction/mod.auction.php */
